4181c0ce16570b219e26eba369ebd67897c3c7b0
joaquin_n6 <joaquinne87@gmail.com>
2024-10-31 21:18:50 -0600

    mejor vista del layout

--- src/layout/Menu.vue
+++ src/layout/Menu.vue
@@ -16,50 +16,61 @@ const handleClose = (key, keyPath) => {
 }
 </script>
 <template>
-  <div>
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-      <el-radio-button :value="false">expand</el-radio-button>
-      <el-radio-button :value="true">collapse</el-radio-button>
-    </el-radio-group>
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      :collapse="isCollapse"
-      @open="handleOpen"
-      @close="handleClose"
-    >
+  <el-scrollbar>
+    <el-menu :default-openeds="['1', '3']">
       <el-sub-menu index="1">
         <template #title>
-          <el-icon><location /></el-icon>
-          <span>Navigator One</span>
+          <el-icon><message /></el-icon>Navigator One
         </template>
         <el-menu-item-group>
-          <template #title><span>Group One</span></template>
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
+          <template #title>Group 1</template>
+          <el-menu-item index="1-1">Option 1</el-menu-item>
+          <el-menu-item index="1-2">Option 2</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="1-3">Option 3</el-menu-item>
         </el-menu-item-group>
         <el-sub-menu index="1-4">
-          <template #title><span>item four</span></template>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
+          <template #title>Option4</template>
+          <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
         </el-sub-menu>
       </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>Navigator Two</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <el-icon><document /></el-icon>
-        <template #title>Navigator Three</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
-      </el-menu-item>
+      <el-sub-menu index="2">
+        <template #title>
+          <el-icon><icon-menu /></el-icon>Navigator Two
+        </template>
+        <el-menu-item-group>
+          <template #title>Group 1</template>
+          <el-menu-item index="2-1">Option 1</el-menu-item>
+          <el-menu-item index="2-2">Option 2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="2-3">Option 3</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="2-4">
+          <template #title>Option 4</template>
+          <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-sub-menu index="3">
+        <template #title>
+          <el-icon><setting /></el-icon>Navigator Three
+        </template>
+        <el-menu-item-group>
+          <template #title>Group 1</template>
+          <el-menu-item index="3-1">Option 1</el-menu-item>
+          <el-menu-item index="3-2">Option 2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group 2">
+          <el-menu-item index="3-3">Option 3</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="3-4">
+          <template #title>Option 4</template>
+          <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
     </el-menu>
-  </div>
+  </el-scrollbar>
 </template>
 
 
