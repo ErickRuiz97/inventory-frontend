f7a03d40bc9ab4dfe2d166c8edef74a3626fe874
joaquin_n6 <joaquinne87@gmail.com>
2024-11-05 22:17:56 -0600

    routes con meta breadcrumb

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,6 @@
 import HomeView from '@/views/HomeView.vue'
 import ProductsList from '@/views/products/ProductsList.vue'
+import ProductDetail from '@/views/product-detail/ProductDetail.vue'
 export const routes = [
   {
     path: '/',
@@ -10,10 +11,26 @@ export const routes = [
     path: '/home',
     name: 'Home',
     component: HomeView,
+    meta: { breadcrumb: 'Home' },
   },
+  //product routes
   {
     path: '/products',
     name: 'Productos',
     component: ProductsList,
+    meta: { breadcrumb: 'Productos' },
+  },
+  {
+    path: '/products/create',
+    name: 'Crear producto',
+    component: ProductDetail,
+    meta: { breadcrumb: 'Crear producto' },
+  },
+  {
+    path: '/products/:id',
+    name: 'Editar producto',
+    component: ProductDetail,
+    props: true,
+    meta: { breadcrumb: 'Editar producto' },
   },
 ]
