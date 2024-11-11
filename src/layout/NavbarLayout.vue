5e6e9c9b1e6cb975ee10fc095f90fe1f9e114f3e
joaquin_n6 <joaquinne87@gmail.com>
2024-11-10 19:47:19 -0600

    cerrar session

--- src/layout/NavbarLayout.vue
+++ src/layout/NavbarLayout.vue
@@ -1,19 +1,33 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const user = ref({})
+
+onMounted(() => {
+  user.value = JSON.parse(localStorage.getItem('user'))
+})
+
+function singOut() {
+  localStorage.removeItem('user')
+  router.push({ name: 'Login' })
+}
 </script>
 <template>
   <div class="toolbar">
+    <span>{{ user.email }}</span>
     <el-dropdown>
-      <el-icon style="margin-right: 8px; margin-top: 1px">
+      <el-icon class="ms-1">
         <setting />
       </el-icon>
       <template #dropdown>
         <el-dropdown-menu>
           <el-dropdown-item>Cambiar contraseña</el-dropdown-item>
-          <el-dropdown-item>Cerrar sesión</el-dropdown-item>
+          <el-dropdown-item @click="singOut">Cerrar sesión</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
-    <span>Tom</span>
   </div>
 </template>
 <style>
