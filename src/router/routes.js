8cba30adaa0ec7a57407f6bc12ecea7682e1e417
jonathan-code-95 <jonathanney14@gmail.com>
2025-03-26 16:55:48 -0600

    agregando rutas de las paginas

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,9 @@
 //General routes
 import LoginApp from '@/views/login/LoginApp.vue'
-//import NoAuth from '@/views/no-auth/NoAuth.vue'
+import NoAuth from '@/views/no-auth/NoAuth.vue'
 
 
-/* 
+
 // Suppliers routes
 import SuppliersList from '@/views/suppliers/SuppliersList.vue'
 import SupplierDetail from '@/views/supplier-detail/SupplierDetail.vue'
@@ -23,7 +23,7 @@ import PurchaseDetail from '@/views/purcharse-detail/PurchaseDetail.vue'
 import ConfigApp from '@/views/config/ConfigApp.vue'
 
 // Change password
-import ChangePassword from '@/views/change-password/ChangePassword.vue' */
+import ChangePassword from '@/views/change-password/ChangePassword.vue'
 import DashboardLayout from '@/layout/DashboardLayout.vue'
 import HomeDashboard from '@/views/home-dashboard/HomeDashboard.vue'
 
@@ -83,8 +83,120 @@ const loginPages = {
       component: LoginApp,
       meta: { public: true },
     },
+    {
+      path: '/change-password',
+      name: 'ChangePassword',
+      component: ChangePassword,
+      meta: { breadcrumb: 'Cambiar Contraseña' },
+    }
   ]
-}
+};
+const usersPages ={
+  path : "/users",
+  redirect : "/users",
+  component : DashboardLayout,
+  children : [
+    {
+      path: '/users',
+      name: 'UserList',
+      component: UsersList,
+      meta: { breadcrumb: 'Usuarios', requiresAuth: ['ADMIN', 'MANAGER'] },
+    },
+    {
+      path: 'create',
+      name: 'UserCreate',
+      component: UserDetail,
+      meta: { breadcrumb: 'Crear usuario', requiresAuth: ['ADMIN', 'MANAGER'] },
+    },
+    {
+      path: ':id',
+      name: 'UserUpdate',
+      component: UserDetail,
+      props: true,
+      meta: { breadcrumb: 'Editar usuario', requiresAuth: ['ADMIN', 'MANAGER'] },
+    },
+  ]
+};
+const supplierPages ={
+  path : "/suppliers",
+  redirect : "/suppliers",
+  component : DashboardLayout,
+  children : [
+    {
+      path: '/suppliers',
+      name: 'SuppliersList',
+      component: SuppliersList,
+      props: true,
+      meta: { breadcrumb: 'Proveedores', requiresAuth: ['ADMIN', 'MANAGER'] },
+    },
+    {
+      path: 'create',
+      name: 'SupplierCreate',
+      component: SupplierDetail,
+      meta: { breadcrumb: 'Crear proveedor', requiresAuth: ['ADMIN', 'MANAGER'] },
+    },
+    {
+      path: ':id',
+      name: 'SupplierEdit',
+      component: SupplierDetail,
+      props: true,
+      meta: {
+        breadcrumb: 'Editar proveedor',
+        requiresAuth: ['ADMIN', 'MANAGER'],
+      },
+    },
+  ]
+};
+const salesPages ={
+  path : "/sales",
+  redirect : "/sales",
+  component : DashboardLayout,
+  children : [
+    {
+      path: '/sales',
+      name: 'SalesList',
+      component: SalesList,
+      meta: { breadcrumb: 'Ventas' },
+    },
+    {
+      path: 'create',
+      name: 'SaleCreate',
+      component: SaleDetail,
+      meta: { breadcrumb: 'Registrar venta' },
+    },
+    {
+      path: ':id',
+      name: 'SaleDetail',
+      component: SaleDetail,
+      meta: { breadcrumb: 'Detalle venta' },
+    },
+  ]
+};
+const purchasesPages ={
+  path : "/purchases",
+  redirect : "/purchases",
+  component : DashboardLayout,
+  children : [
+    {
+      path: '/purchases',
+      name: 'PurchasesList',
+      component: PurchasesList,
+      meta: { breadcrumb: 'Compras' },
+    },
+    {
+      path: 'create',
+      name: 'PurchaseCreate',
+      component: PurchaseDetail,
+      meta: { breadcrumb: 'Registrar compra' },
+    },
+    {
+      path: ':id',
+      name: 'PurchaseDetail',
+      component: PurchaseDetail,
+      meta: { breadcrumb: 'Detalle compra' },
+    },
+  ]
+};
 export const routes = [
   {
     path:"/",
@@ -92,7 +204,20 @@ export const routes = [
     name:"Dashboard",
     meta: { public: true },
   },
+  {
+    path: '/no-auth',
+    name: 'NoAuth',
+    component: NoAuth,
+    props: true,
+    meta: {
+      breadcrumb: 'No autorizado',
+    },
+  },
   dashboardPages,
   productPages,
+  usersPages,
+  supplierPages,
+  salesPages,
+  purchasesPages,
   loginPages
 ]
