f1048b5dcbd73790833c5ea02bb643d509ae6ba2
joaquin_n6 <joaquinne87@gmail.com>
2025-03-30 18:36:00 -0600

    Refactor DrawerMenu.vue for improved readability and consistency

--- src/layout/DrawerMenu.vue
+++ src/layout/DrawerMenu.vue
@@ -1,32 +1,41 @@
 <script setup>
-import { ref } from "vue";
-import MenuOptions from "./components/MenuOptions.vue";
+import { ref } from 'vue'
+import MenuOptions from './components/MenuOptions.vue'
 
-const drawerVisible = ref(false);
-const props = defineProps({
+const drawerVisible = ref(false)
+defineProps({
   isMobile: Boolean,
-});
+})
 const closeDrawer = () => {
-  drawerVisible.value = false;
-};
+  drawerVisible.value = false
+}
 const toggleDrawer = () => {
-  drawerVisible.value = !drawerVisible.value;
-};
-
+  drawerVisible.value = !drawerVisible.value
+}
 </script>
 <template>
   <div class="position-relative">
-    <el-button @click="toggleDrawer" class="menu-button position-absolute top-20 start-20" v-if="isMobile">
-      <el-icon><Menu/></el-icon>
+    <el-button
+      @click="toggleDrawer"
+      class="menu-button position-absolute top-20 start-20"
+      v-if="isMobile"
+    >
+      <el-icon><Menu /></el-icon>
     </el-button>
   </div>
-  <el-drawer style="background-color: #e0e0e0" size="50%" v-model="drawerVisible" title="Menú" direction="ltr" >
-    <menu-options  @select="closeDrawer"/>
+  <el-drawer
+    style="background-color: #e0e0e0"
+    size="50%"
+    v-model="drawerVisible"
+    title="Menú"
+    direction="ltr"
+  >
+    <menu-options @select="closeDrawer" />
   </el-drawer>
 </template>
 
 <style lang="scss" scoped>
-.ep-drawer  {
-  width: 50% !important;;
+.ep-drawer {
+  width: 50% !important;
 }
 </style>
